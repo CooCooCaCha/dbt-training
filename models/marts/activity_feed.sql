1c5b1f918782b1c6b5c6d512fa1adc1737ec7e7a
Bill Johnson <bill.johnson@world50.com>
2021-11-13 16:29:14 +0000

    Export personid for filtering

--- models/marts/activity_feed.sql
+++ models/marts/activity_feed.sql
@@ -1,11 +1,36 @@
-SELECT
-  ID,
-  'Post' as ENTITY,
-  (case when createdAtOverride is not null then createdAtOverride else createdAt end) as TIMESTAMP,
-  object_construct(*) as DATA
-FROM RAW_DATALAKE.DEVLIVE_W50POSTGRES.POST
-WHERE PERSONAID IN (
-  SELECT ID
-  FROM RAW_DATALAKE.DEVLIVE_W50POSTGRES.PERSONA
-  WHERE PERSONAID IN ('efb43ddc-1946-4a1b-995d-4f285864db20')
+SELECT * 
+FROM (
+    SELECT
+        ID,
+        'Post' as ENTITY,
+        (
+            case 
+                when createdAtOverride is not null 
+                then createdAtOverride 
+                else createdAt 
+            end
+        ) as TIMESTAMP,
+        (
+            SELECT ANY_VALUE(p.PERSONAID)
+            FROM RAW_DATALAKE.DEVLIVE_W50POSTGRES.PERSONA p 
+            WHERE p.ID = post.PERSONAID
+        ) as PERSONID,
+        object_construct(*) as DATA
+    FROM RAW_DATALAKE.DEVLIVE_W50POSTGRES.POST post
+
+    UNION ALL
+
+    SELECT
+        ID,
+        'Media' as ENTITY,
+        createdAt as TIMESTAMP,
+        (
+            SELECT ANY_VALUE(PERSONID)
+            FROM RAW_DATALAKE.DEVLIVE_W50POSTGRES.USER
+            WHERE ID = ums.USERID
+        ) as PERSONID,
+        object_construct(*) as DATA
+    FROM RAW_DATALAKE.DEVLIVE_W50POSTGRES.user_media_state ums
 )
+WHERE PERSONID IN ('efb43ddc-1946-4a1b-995d-4f285864db20')
+ORDER BY TIMESTAMP DESC
